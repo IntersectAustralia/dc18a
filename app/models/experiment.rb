c8ad87632b4a2817028097787f2622726dbda553
veronica <veronica@intersect.org.au>
2012-08-31 16:18:47 +1000

    Added validations for Experiment model

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -6,4 +6,16 @@ class Experiment < ActiveRecord::Base
   attr_accessible :cell_type_or_tissue, :lab_book_no, :expt_name, :page_no, :expt_type, :project_id ,
                   :slides, :dishes, :other, :other_text, :reporter_protein, :reporter_protein_text,
                   :specific_dyes, :specific_dyes_text, :immunofluorescence, :created_date
+
+  validates_length_of :expt_name, :maximum => 255
+  validates_length_of :lab_book_no, :maximum => 255
+  validates_length_of :page_no, :maximum => 255
+  validates_length_of :cell_type_or_tissue, :maximum => 255
+  validates_length_of :reporter_protein, :maximum => 255
+  validates_length_of :specific_dyes, :maximum => 255
+  validates_presence_of :expt_type
+  validates_presence_of :other_text, :if => Proc.new { |expt| expt.other?}
+  validates_presence_of :reporter_protein_text, :if => Proc.new { |expt| expt.reporter_protein? }
+  validates_presence_of :specific_dyes_text, :if => Proc.new { |expt| expt.specific_dyes? }
+
 end
