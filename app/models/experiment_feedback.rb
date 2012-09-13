a512b920de1744f8a23d628f5d4bd7a6ee6b01d7
veronica <veronica@intersect.org.au>
2012-09-13 16:33:47 +1000

    Added send email when instrument fail feature

--- app/models/experiment_feedback.rb
+++ app/models/experiment_feedback.rb
@@ -7,8 +7,18 @@ class ExperimentFeedback < ActiveRecord::Base
   validates_presence_of :instrument_failed
   validates_presence_of :instrument_failed_reason, :if => :instrument_failed?, :message => "You must specify an instrument failure reason if the instrument failed."
 
+  after_commit :notify_admins_if_instrument_failed, :on => create
 
   def to_json_data(success)
     { "success" => success }
   end
+
+  private
+
+  def notify_admins_if_instrument_failed
+    if self.instrument_failed?
+      Notifier.notify_admins_of_instrument_failure(self).deliver
+    end
+  end
+
 end
