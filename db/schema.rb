5a9325fa09f89c167ee30f8858a8415321897288
veronica <veronica@intersect.org.au>
2012-09-21 14:17:44 +1000

    DC18A-83 Changed schema after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120917054943) do
+ActiveRecord::Schema.define(:version => 20120921012248) do
 
   create_table "experiment_feedbacks", :force => true do |t|
     t.datetime "created_at",               :null => false
