6c93202c2bf02e64a1263d1fa88b457aa978ab59
Sean Lin <seanl@intersect.org.au>
2012-10-18 13:29:11 +1100

    Tidy up deploy.rb to get around syntax error in RubyMine

--- config/deploy.rb
+++ config/deploy.rb
@@ -249,5 +249,5 @@ task :generate_database_yml, :roles => :app do
 end
 
 after 'multistage:ensure' do
-  set (:rails_env) {"#{defined?(rails_env) ? rails_env : stage.to_s}" }
+  set :rails_env, proc { "#{defined?(rails_env) ? rails_env : stage.to_s}" }
 end
