0db2544cf1eb5ba4c93bc6a66fc2a16e8e59842c
Sean Lin <seanl@intersect.org.au>
2012-10-23 09:17:33 +1100

    Set default user runs rails app in production environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,4 +4,6 @@ role :web, '172.16.4.92'
 role :app, '172.16.4.92'
 # This is where Rails migrations will run
 role :db,  '172.16.4.92', :primary => true
+# The user configured to run the rails app
+set :user, 'dc18a'
 
