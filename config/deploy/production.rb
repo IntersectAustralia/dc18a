2943d0efd8eda56b892ccf6339a2a718f55c4661
Sean Lin <seanl@intersect.org.au>
2013-02-11 21:45:01 +1100

    Deployment: test ldap environment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
 # Your HTTP server, Apache/etc
-role :web, '172.16.4.92'
+role :web, '138.25.37.201'
 # This may be the same as your Web server
-role :app, '172.16.4.92'
+role :app, '138.25.37.201'
 # This is where Rails migrations will run
-role :db,  '172.16.4.92', :primary => true
+role :db,  '138.25.37.201', :primary => true
 # The user configured to run the rails app
 set :user, 'dc18a'
 
