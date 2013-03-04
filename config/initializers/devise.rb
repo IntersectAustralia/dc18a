ebb530c82bcea72c3f6e9fd1a90d0c45a22642a2
Sean Lin <seanl@intersect.org.au>
2013-03-05 09:00:20 +1100

    Set admin binding if search agent is not admin, which does not return DN

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # config.ldap_config = "#{Rails.root}/config/ldap.yml"
   # config.ldap_check_group_membership = false
   # config.ldap_check_attributes = false
-  # config.ldap_use_admin_to_bind = false
+  config.ldap_use_admin_to_bind = true
   
   # http://stackoverflow.com/questions/580314/overriding-a-module-method-from-a-gem-in-rails/1852448#1852448
   # verifies password before checking if account is active
