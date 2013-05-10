3990a92590c273e76becb8f817d58c8db113a8c0
Gabriel E. Gasser Noblia <gabriel@intersect.org.au>
2013-05-10 16:15:40 +1000

    DEVSUPPORT-1007: Remove presence validation on 2 fields of experiment model.

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -18,14 +18,14 @@ class Experiment < ActiveRecord::Base
   validates_length_of :cell_type_or_tissue, :maximum => 255
   validates_length_of :other_text, :maximum => 255
   validates_presence_of :expt_name
-  validates_presence_of :lab_book_no
-  validates_presence_of :page_no
+  #validates_presence_of :lab_book_no
+  #validates_presence_of :page_no
   validates_presence_of :cell_type_or_tissue
   validates_presence_of :expt_type
   validates_presence_of :other_text, :if => :other?, :message => '"Other (Specify)" cannot be empty if "Other" is checked'
   validates_presence_of :fluorescent_protein_ids, if: :has_fluorescent_proteins?, message: "can't be empty if 'Fluorescent proteins' is checked"
   validates_presence_of :specific_dye_ids, if: :has_specific_dyes?, message: "can't be empty if 'Specific dyes' is checked"
-  validates_presence_of :immunofluorescence_ids, if: :has_immunofluorescence?, message: "can't be empty if 'Immunofluorescence' is checked"
+  validates_presence_of :immunofluorescence_ids, if: :has_immunofluorescence?, message: "can't be empty if 'Secondary Antibodies' is checked"
 
   before_save :assign_experiment_id
 
