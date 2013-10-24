83565d549cb5519027d51f11ee4999d3cff52efd
Sean Lin <seanl@intersect.org.au>
2013-10-24 13:11:39 +1100

    Externalize ldap.yml to local shared file, deploy:setup picks up it and upload to server, then create symlink for each release

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ require 'capistrano/ext/multistage'
 require 'capistrano_colors'
 require 'rvm/capistrano'
 
+set :shared_file_dir, "files"
+set(:shared_file_path) { File.join(shared_path, shared_file_dir) }
+
 set :application, 'dc18a'
 set :stages, %w(qa staging production)
 set :default_stage, "qa"
@@ -236,6 +239,54 @@ namespace :deploy do
   task :new_secret, :roles => :app do
     run("cd #{current_path} && rake app:generate_secret", :env => {'RAILS_ENV' => "#{stage}"})
   end
+
+  namespace :shared_file do
+
+    desc <<-DESC
+      Generate shared file dirs under shared/files dir and then copies files over.
+
+      For example, given:
+        set :shared_files, %w(config/database.yml db/seeds.yml)
+
+      The following directories will be generated:
+        shared/files/config/
+        shared/files/db/
+    DESC
+
+    task :setup, :except => { :no_release => true } do
+      if exists?(:shared_files)
+        dirs = shared_files.map {|f| File.join(shared_file_path, File.dirname(f)) }
+        run "#{try_sudo} mkdir -p #{dirs.join(' ')}"
+        run "#{try_sudo} chmod g+w #{dirs.join(' ')}" if fetch(:group_writable, true)
+        run "#{try_sudo} chown -R #{user}.#{group} #{dirs.join(' ')}"
+
+        servers = find_servers(:no_release => false)
+        servers.each do |server|
+          shared_files.each do |file_path|
+            top.upload(file_path, File.join(shared_file_path, file_path))
+            puts "    Uploaded #{file_path} to #{File.join(shared_file_path, file_path)}"
+          end
+        end
+
+      end
+    end
+    after "deploy:setup", "deploy:shared_file:setup"
+
+    desc <<-DESC
+      Symlink shared files to release path.
+
+      WARNING: It DOES NOT warn you when shared files not exist.  \
+      So symlink will be created even when a shared file does not \
+      exist.
+    DESC
+    task :create_symlink, :except => { :no_release => true } do
+      (shared_files || []).each do |path|
+        run "ln -nfs #{shared_file_path}/#{path} #{release_path}/#{path}"
+      end
+    end
+    after "deploy:finalize_update", "deploy:shared_file:create_symlink"
+  end
+
 end
 
 namespace :backup do
@@ -290,4 +341,9 @@ end
 
 after 'multistage:ensure' do
   set :rails_env, proc { "#{defined?(rails_env) ? rails_env : stage.to_s}" }
+end
+
+after 'multistage:ensure' do
+  set(:rails_env) { "#{defined?(rails_env) ? rails_env : stage.to_s}" }
+  set :shared_files, %W(config/ldap.yml)
 end
